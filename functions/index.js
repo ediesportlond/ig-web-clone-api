21686dafc7f58dab7e48071149cd74b5b99320f4
Eddie Espinosa <espinosa.edilson@gmail.com>
2022-11-17 17:21:56 -0500

    like routes

--- functions/index.js
+++ functions/index.js
@@ -2,11 +2,16 @@ import functions from 'firebase-functions';
 import express from 'express';
 import cors from 'cors';
 
-import { getAllPhotos, addNewPhoto } from './src/photos.js';
+import { getAllPhotos, addNewPhoto, addLikes, getLikes } from './src/photos.js';
 const app = express();
 //any time you do anything other than get
 app.use(express.json());
 app.use(cors());
+
+//get will return if user has liked it.
+app.get('/likes/:postId/:userId', getLikes);
+//post will add their like if not in it. and increase like counter
+app.post('/likes/:postId/:userId', addLikes);
 
 app.get('/photos', getAllPhotos);
 app.post('/photos', addNewPhoto);
--- functions/src/photos.js
+++ functions/src/photos.js
@@ -1,4 +1,5 @@
 import dbConnect from './dbConnect.js';
+import {FieldValue} from 'firebase-admin/firestore'
 
 export async function getAllPhotos(req, res) {
   const db = dbConnect();
@@ -23,5 +24,90 @@ export async function addNewPhoto(req, res) {
       return;
     })
 
-    getAllPhotos(req, res);
+  getAllPhotos(req, res);
+}
+
+export async function addLikes(req, res) {
+  //get postId and userId from params
+  const { userId, postId } = req.params
+  //get this post by postId
+  const db = dbConnect();
+
+  const result = await db.collection("photos").doc(postId).get()
+    .catch(err => {
+      res.status(500).send({ success: false, message: err });
+      return;
+    })
+
+  const photo = result.data()
+
+  //see if userId exists in like object
+  if (photo.likes[userId]) {
+    
+    //if it does remove it
+    delete photo.likes[userId]
+    delete photo.likeCount
+
+    //then minus from count
+    await db.collection("photos").doc(postId).update(photo)
+      .catch(err => {
+        res.status(500).send({ success: false, message: err });
+        return;
+      })
+
+    await db.collection("photos").doc(postId).update({
+      likeCount: FieldValue.increment(-1)
+    })
+      .catch(err => {
+        res.status(500).send({ success: false, message: err });
+        return;
+      })
+
+
+      res.send({isLiked:false, likes: photo.likeCount -1})
+      return
+  } else {
+    //if it doesn't add it
+    photo.likes[userId] = true
+    delete photo.likeCount
+    //then add to coount
+    await db.collection("photos").doc(postId).update({
+      likeCount: FieldValue.increment(1)
+    })
+      .catch(err => {
+        res.status(500).send({ success: false, message: err });
+        return;
+      })
+
+    await db.collection("photos").doc(postId).update(photo)
+      .catch(err => {
+        res.status(500).send({ success: false, message: err });
+        return;
+      })
+      res.send({isLiked:true, likes: photo.likeCount +1})
+      return
+  }
+}
+
+export async function getLikes(req, res) {
+  //get postId and userId from params
+  const { userId, postId } = req.params
+
+  //get this post by postId
+  const db = dbConnect();
+
+  const result = await db.collection("photos").doc(postId).get()
+    .catch(err => {
+      res.status(500).send({ success: false, message: err });
+      return;
+    })
+
+  const photo = result.data()
+  //see if userId exists in like object
+  if (photo.likes.userId) {
+    res.send({isLiked: true, likes: photo.likeCount})
+  } else {
+    res.send({isLiked: false, likes: photo.likeCount})
+  }
+  //return {isLiked: boolean, likes: num}
 }
